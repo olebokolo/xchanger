e22c396a775dfc47a391de477e768f586d9b4503
Alexander <alexander@pioneerlabs.io>
2019-06-13 20:21:21 +0200

    Added async to app.test.tsx

--- src/app/app.tst.tsx
+++ src/app/app.tst.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './app'
 
-it('renders without crashing', () => {
+it('renders without crashing', async () => {
   const div = document.createElement('div')
   ReactDOM.render(<App />, div)
   ReactDOM.unmountComponentAtNode(div)
